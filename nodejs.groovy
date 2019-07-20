2176b77d8d456ebcb0bcd26ba51a2c15c42b03f6
Luku <luku.3659@gmail.com>
2019-07-21 03:02:03 +0900

    add scripts start

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,17 +1,9 @@
 job('NodeJS Job') {
-    scm {
-        git('https://github.com/knysyy/jenkins-selenium.git') {
-            node / gitConfigName('DSL User')
-            node / gitConfigName('jenkins-dsl@example.com')
-        }
-    }
-    triggers {
-        scm('H/5 * * * *')
-    }
     wrappers {
         nodejs('Node_v12')
     }
     steps {
         shell("npm install")
+        shell("npm start")
     }
 }
